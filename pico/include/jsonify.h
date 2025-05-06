458eb4205f2dfac0e6a59870cc391527019bae94
Benji <ben.flodin@outlook.com>
2025-05-06 14:02:45 +0200

    Update jsonify.h

--- pico/include/jsonify.h
+++ pico/include/jsonify.h
@@ -1,6 +1,6 @@
 #ifndef JSONIFY_H
 #define JSONIFY_H
 
-char* create_json(char* user_id, char* date, char* time);
+char* create_json(char* user_id, char* date, char* time, float distance);
 
 #endif
