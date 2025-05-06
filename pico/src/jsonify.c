7d11b32410240446f0a6f3ba8c88b977c354e345
Benji <ben.flodin@outlook.com>
2025-05-06 14:04:07 +0200

    Update jsonify.c

--- pico/src/jsonify.c
+++ pico/src/jsonify.c
@@ -1,11 +1,11 @@
 #include "jsonify.h"
 #include <stdio.h>
 
-char* create_json(const char* user_id, char* date, char* time){
+char* create_json(const char* user_id, char* date, char* time, float distance){
         static char json_data[256];
 
         snprintf(json_data, sizeof(json_data),
-                "{\"user_id\": \"%s\", \"date\": \"%s\", \"time\": \"%s\"}",
+                "{\"user_id\": \"%s\", \"date\": \"%s\", \"time\": \"%s\", \"distance\": \"%f\"}",
                 user_id, date, time);
         return json_data;
 }
