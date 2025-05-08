6c33809165602534d930db5fec2ed087ddf864ea
Narunya <abzakhnartan@gmail.com>
2025-05-08 09:22:09 +0200

    fix

--- pico/src/wifi.c
+++ pico/src/wifi.c
@@ -10,10 +10,11 @@ void connect_to_wifi(){
 
 	printf("Attempting to connect to wifi ...");
 	
-	if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASS, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+	while (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASS, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
 		printf("Failed to connect ..-");
+		sleep_ms(3000);
 	}
-	else {
-		printf("Connected to wifi: %s\n", WIFI_SSID);
-	}
+	
+	printf("Connected to wifi: %s\n", WIFI_SSID);
+	
 }
